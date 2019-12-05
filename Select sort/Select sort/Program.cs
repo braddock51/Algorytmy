2446e64581b248cf56f7c3e1c9e396d10d77f133
braddock51 <47740798+braddock51@users.noreply.github.com>
2019-12-05 10:47:10 +0100

    Add files via upload

---
+++ Select sort/Select sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Select_sort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int []tab = new int [5] {4,3,2,1,5};
+
+            foreach (int i in tab)
+                Console.Write($"{i},");
+
+            
+            for (int i = 0; i < tab.Length-1; i++)
+            {
+                int imin = i;
+                for(int j=i+1; j<tab.Length; j++)
+                {
+                    if (tab[j] < tab[imin])
+                    { imin = j; }
+                    
+                }
+                if (imin != i)
+                {
+
+                    int temp = tab[i];
+                    tab[i] = tab[imin];
+                    tab[imin] = temp;
+                }
+
+            }
+            Console.WriteLine("\n");
+            
+            foreach (int i in tab)
+                Console.Write($"{i},");
+            Console.ReadLine();
+        }
+    }
+}
