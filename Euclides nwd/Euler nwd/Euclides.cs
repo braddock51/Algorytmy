3b4281d2c8a6f35811d005583e5665ec57b828ed
braddock51 <47740798+braddock51@users.noreply.github.com>
2019-12-05 10:43:37 +0100

    Add files via upload

---
+++ Euclides nwd/Euler nwd/Euclides.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Euler_nwd
+{
+    public class Euclides
+    {
+        public static int Nwd(int a, int b)
+        {
+            int temp = a % b;
+            if (temp == 0)
+                return b;
+            return Nwd(b, temp);
+            
+        }
+    }
+}
---
+++ Euclides nwd/Euler nwd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Euler_nwd
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Euclides.Nwd(22, 44));
+
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
