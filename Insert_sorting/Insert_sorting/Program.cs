a626e61b6e1f4991923e3ce02a9510457b8a3866
braddock51 <47740798+braddock51@users.noreply.github.com>
2019-12-05 10:46:38 +0100

    Add files via upload

---
+++ Insert_sorting/Insert_sorting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insert_sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[5] { 5, 1, 4, 3, 5 };
+            foreach (int i in tab)
+                Console.Write(i);
+
+            for(int i=1; i<tab.Length; i++)
+            {
+                int key = tab[i];
+                int j = i - 1;
+                while (j > -1 && tab[j]>key)
+                {
+                    tab[j+1] = tab[j];
+                    tab[j] = key;
+                    j--;
+                }
+            }
+            Console.Write("\n");
+            foreach (int i in tab)
+                Console.Write(i);
+
+            Console.ReadLine();
+        }
+    }
+}
