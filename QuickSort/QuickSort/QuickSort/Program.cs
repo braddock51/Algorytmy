377e11fb59b955c9ccfe0c2c1c02117cc07435ff
braddock51 <47740798+braddock51@users.noreply.github.com>
2020-02-05 09:35:51 +0100

    Add files via upload

---
+++ QuickSort/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] tab = new int[5] { -4, 2, 4, 11, -10 };
+
+            QuickSort(tab, 0, tab.Length - 1);
+
+            foreach (var item in tab)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            var i = left;
+            var j = right;
+            var pivot = array[(left + right) / 2];
+            while (i < j)
+            {
+                while (array[i] < pivot) i++;
+                while (array[j] > pivot) j--;
+                if (i <= j)
+                {
+                    // swap
+                    var tmp = array[i];
+                    array[i++] = array[j];  // ++ and -- inside array braces for shorter code
+                    array[j--] = tmp;
+                }
+            }
+            if (left < j) QuickSort(array, left, j);
+            if (i < right) QuickSort(array, i, right);
+        }
+    }
+}
